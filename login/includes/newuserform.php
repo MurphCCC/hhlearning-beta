04d5bf4fdda84ba9145ea020fd39606e75a04d4b
Michael Conrad <murph@Michaels-MacBook-Pro.local>
2017-11-01 17:37:08 -0400

    Refactored code to add random hash to database when user is created, used for password reset logic

--- login/includes/newuserform.php
+++ login/includes/newuserform.php
@@ -41,69 +41,8 @@ class NewUserForm extends DbConn
 
     }
 
-        public function resetPass($teacher_id, $password )
-    {
-        try {
-
-            $db = new DbConn;
-            $tbl_members = 'teachers';
-            // prepare sql and bind parameters
-            $stmt = $db->conn->prepare("UPDATE TABLE ".$tbl_members." SET `password` = :pass WHERE `teacher_id` = :tid");
-            $stmt->bindParam(':pass', $pass);
-            $stmt->bindParam(':tid', $tid);
-            $stmt->execute();
-
-            $err = '';
-
-        } catch (PDOException $e) {
-
-            $err = "Error: " . $e->getMessage();
-
-        }
-        //Determines returned value ('true' or error code)
-        if ($err == '') {
-
-            $success = 'true';
-
-        } else {
-
-            $success = $err;
-
-        };
-
-        return $success;
-
-    }
-}
-
-Class ResetUser extends DbConn
-{
-    public function resetPass($uid, $pass) 
-    {
-        $pass = password_hash($pass, PASSWORD_DEFAULT);
-        try {
-            $vdb = new DbConn;
-            $tbl_members = $vdb->tbl_members;
-            $verr = '';
-
-        // prepare sql and bind parameters
-        $vstmt = $vdb->conn->prepare('UPDATE '.$tbl_members.' SET password = :pass WHERE hash = :uid');
-            $vstmt->bindParam(':uid', $uid);
-            $vstmt->bindParam(':pass', $pass);
-            $vstmt->execute();
-
-        } catch (PDOException $v) {
-
-            $verr = 'Error: ' . $v->getMessage();
-
-        }
-
-    //Determines returned value ('true' or error code)
-    $resp = ($verr == '') ? 'true' : $verr;
-
-        return $resp;
-
-    }
 
 }
+
+
     
--- login/includes/verify.php
+++ login/includes/verify.php
@@ -27,30 +27,5 @@ class Verify extends DbConn
 
     }
 
-    public function resetPass($uid, $pass) 
-    {
-        $pass = password_hash($pass, PASSWORD_DEFAULT);
-        try {
-            $vdb = new DbConn;
-            $tbl_members = $vdb->tbl_members;
-            $verr = '';
 
-        // prepare sql and bind parameters
-        $vstmt = $vdb->conn->prepare('UPDATE '.$tbl_members.' SET password = :pass WHERE hash = :uid');
-            $vstmt->bindParam(':uid', $uid);
-            $vstmt->bindParam(':pass', $pass);
-            $vstmt->execute();
-
-        } catch (PDOException $v) {
-
-            $verr = 'Error: ' . $v->getMessage();
-
-        }
-
-    //Determines returned value ('true' or error code)
-    $resp = ($verr == '') ? 'true' : $verr;
-
-        return $resp;
-
-    }
 }
